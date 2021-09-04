be9bd0af4245edb929e240cfc28bcd8c389c94d8
professor-oats <chronophob@gmail.com>
2021-09-04 12:38:16 +0200

    bouncing_spheres

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "moving_sphere.h"
 
 #include <iostream>
 
@@ -47,7 +48,8 @@ hittable_list random_scene() {
 		    // diffuse
 		    auto albedo = color::random() * color::random();
 		    sphere_material = make_shared<lambertian>(albedo);
-		    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+		    auto center2 = center + vec3(0, random_double(0, 0.5), 0);
+		    world.add(make_shared<moving_sphere>(center, center2, 0.0, 1.0, 0.2, sphere_material));
 		} else if (choose_mat < 0.95) {
 		    // metal
 		    auto albedo = color::random(0.5, 1);
@@ -79,7 +81,7 @@ int main() {
 
     // Image
 
-    const auto aspect_ratio = 3.0 / 2.0;
+    const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 20;
@@ -97,7 +99,7 @@ int main() {
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
 
     // Render
 
