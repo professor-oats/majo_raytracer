39702b5ede14b9220bc6dc7f3a0adad0117e5fbd
professor-oats <chronophob@gmail.com>
2021-09-11 11:19:04 +0200

    checker_texture

--- material.h
+++ material.h
@@ -2,6 +2,7 @@
 #define MATERIAL_H
 
 #include "rtweekend.h"
+#include "texture.h"
 
 struct hit_record;
 
@@ -14,7 +15,8 @@ class material {
 
 class lambertian : public material {
     public:
-	lambertian(const color& a) : albedo(a) {}
+	lambertian(const color& a) : albedo(make_shared<solid_color>(a)) {}
+	lambertian(shared_ptr<texture> a) : albedo(a) {}
 
 	virtual bool scatter(
 	    const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const override {
@@ -27,12 +29,12 @@ class lambertian : public material {
 	    }
 
 	    scattered = ray(rec.p, scatter_direction, r_in.time());
-	    attenuation = albedo;
+	    attenuation = albedo->value(rec.u, rec.v, rec.p);
 	    return true;
 	}
 
     public:
-	color albedo;
+	shared_ptr<texture> albedo;
 };
 
 class metal : public material {
--- moving_sphere.h
+++ moving_sphere.h
@@ -2,7 +2,7 @@
 #define MOVING_SPHERE_H
 
 #include "rtweekend.h"
-
+#include "aabb.h"
 #include "hittable.h"
 
 class moving_sphere : public hittable {
@@ -11,6 +11,9 @@ class moving_sphere : public hittable {
 	moving_sphere(point3 cen0, point3 cen1, double _time0, double _time1, double r, shared_ptr<material> m) : center0(cen0), center1(cen1), time0(_time0), time1(_time1), radius(r), mat_ptr(m) {};
 
 	virtual bool hit(const ray& r, double t_min, double t_max, hit_record& rec) const override;
+
+	virtual bool bounding_box(
+	    double _time0, double _time1, aabb& output_box) const override;
 
 	point3 center(double time) const;
 
@@ -50,6 +53,17 @@ bool moving_sphere::hit(const ray& r, double t_min, double t_max, hit_record& re
     rec.set_face_normal(r, outward_normal);
     rec.mat_ptr = mat_ptr;
 
+    return true;
+}
+
+bool moving_sphere::bounding_box(double _time0, double _time1, aabb& output_box) const {
+    aabb box0(
+	center(_time0) - vec3(radius, radius, radius),
+	center(_time0) + vec3(radius, radius, radius));
+    aabb box1(
+	center(_time1) - vec3(radius, radius, radius),
+	center(_time1) + vec3(radius, radius, radius));
+    output_box = surrounding_box(box0, box1);
     return true;
 }
 
