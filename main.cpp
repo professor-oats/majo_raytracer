6c67673a1dcadc7483fb04f907dc7a71bfb25100
professor-oats <chronophob@gmail.com>
2021-09-15 06:12:42 +0200

    emit_light_rectangle

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 #include "camera.h"
 #include "material.h"
 #include "moving_sphere.h"
+#include "aarect.h"
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray& r, const color& background, const hittable& world, int depth) {
     hit_record rec;
 
     // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -17,18 +18,36 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 	return color(0,0,0);
     }
 
-    if (world.hit(r, 0.001, infinity, rec)) {
+    // If the ray hits nothing, return the background color.
+    if (!world.hit(r, 0.001, infinity, rec)) {
+	return background;
+    }
+
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered)){
+	return emitted;
+    }
+
+    return emitted + attenuation * ray_color(scattered, background, world, depth-1);
+} 
+
+
+/*    if (world.hit(r, 0.001, infinity, rec)) {
 	ray scattered;
 	color attenuation;
 	if (rec.mat_ptr->scatter(r, rec, attenuation, scattered)){
 	    return attenuation * ray_color(scattered, world, depth-1);
 	}
 	return color(0,0,0);
-    }
+ 
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
-}
+} */
 
 hittable_list random_scene() {
     hittable_list world;
@@ -99,11 +118,24 @@ hittable_list two_perlin_spheres() {
 }
 
 hittable_list earth() {
-    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto earth_texture = make_shared<image_texture>("Scraps/earthmap.jpg");
     auto earth_surface = make_shared<lambertian>(earth_texture);
     auto globe = make_shared<sphere>(point3(0,0,0), 2, earth_surface);
 
     return hittable_list(globe);
+}
+
+hittable_list simple_light() {
+    hittable_list objects;
+
+    auto pertext = make_shared<noise_texture>(4);
+    objects.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    objects.add(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+
+    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    objects.add(make_shared<xy_rect>(3, 5, 1, 3, -2, difflight));
+
+    return objects;
 }
 
 int main() {
@@ -118,10 +150,12 @@ int main() {
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
+    color background(0,0,0);
 
     switch (0) {
 	case 1:
 	    world = random_scene();
+	    background = color(0.70, 0.80, 1.00);
 	    lookfrom = point3(13,2,3);
 	    lookat = point3(0,0,0);
 	    vfov = 20.0;
@@ -130,6 +164,7 @@ int main() {
 
 	case 2:
 	    world = two_spheres();
+	    background = color(0.70, 0.80, 1.00);
 	    lookfrom = point3(13,2,3);
 	    lookat = point3(0,0,0);
 	    vfov = 20.0;
@@ -137,16 +172,27 @@ int main() {
 
 	case 3:
 	    world = two_perlin_spheres();
+	    background = color(0.70, 0.80, 1.00);
+	    lookfrom = point3(13,2,3);
+	    lookat = point3(0,0,0);
+	    vfov = 20.0;
+	    break;
+
+	case 4:
+	    world = earth();
+	    background = color(0.70, 0.80, 1.00);
 	    lookfrom = point3(13,2,3);
 	    lookat = point3(0,0,0);
 	    vfov = 20.0;
 	    break;
 
 	default:
-	case 4:
-	    world = earth();
-	    lookfrom = point3(13,2,3);
-	    lookat = point3(0,0,0);
+	case 5:
+	    world = simple_light();
+	    //samples_per_pixel = 20;
+	    background = color(0,0,0);
+	    lookfrom = point3(26,3,6);
+	    lookat = point3(0,2,0);
 	    vfov = 20.0;
 	    break;
     }
@@ -178,7 +224,7 @@ int main() {
 	        auto u = (i + random_double()) / (image_width-1);
 	        auto v = (j + random_double()) / (image_height-1);
 	        ray r(cam.get_ray(u, v));
-	        pixel_color += ray_color(r, world, max_depth);
+	        pixel_color += ray_color(r, background, world, max_depth);
 	    }
 	write_color(std::cout, pixel_color, samples_per_pixel);
         }
